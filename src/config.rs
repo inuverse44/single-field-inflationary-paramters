63be2886ec7645f8b2fa9cc1df9b1f8fc603a149
TatsukiKodama8 <kodama_tatsuki@retail-ai.jp>
2025-09-18 18:25:47 +0900

    add: config.rs

---
+++ src/config.rs
@@ -0,0 +1,25 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub output_file: String, 
+    pub simulations: Vec<Simulation>, 
+}
+
+#[derive(Deserialize)]
+pub struct Simulation {
+    pub name: String, 
+    pub model: String, 
+    pub scan_parameter: Scan, 
+    pub fixed_parameter: HashMap<String, f64>, 
+}
+
+#[derive(Deserialize)]
+pub struct Scan {
+    pub name: String, 
+    pub start: f64, 
+    pub end: f64, 
+    pub steps: usize, 
+}
+
